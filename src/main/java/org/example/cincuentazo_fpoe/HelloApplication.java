c5acf19b0f1f05fe230fcb3015219b5ff96036a8
Edward Toro <edward.toro@correounivalle.edu.co>
2026-07-16 14:25:34 -0500

    Corrige rutas de recursos y arranque de la aplicacion

--- src/main/java/org/example/cincuentazo_fpoe/HelloApplication.java
+++ src/main/java/org/example/cincuentazo_fpoe/HelloApplication.java
@@ -7,12 +7,23 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * Punto de entrada de la aplicacion JavaFX. Carga la pantalla de
+ * configuracion inicial (HU-1), donde el jugador elige cuantos
+ * jugadores maquina participaran.
+ *
+ * @author Nicolas Velasco, Daniel Toro - Cincuentazo
+ */
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                getClass().getResource("/org/example/cincuentazo_fpoe/configuracion.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        scene.getStylesheets().add(
+                getClass().getResource("/org/example/cincuentazo_fpoe/styles.css").toExternalForm());
+        stage.setTitle("Cincuentazo");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
